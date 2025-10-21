204a402fbdf974ac6db23f9ec3c79fb9436be8e0
Victor Xavier <Victorxavier.oliver@gmail.com>
2025-10-20 22:44:11 -0300

    feat(resilience): implement retry with Resilience4j and configure timeout

--- authuser/src/main/java/com/victorxavier/course_platform/authuser/clients/CourseClient.java
+++ authuser/src/main/java/com/victorxavier/course_platform/authuser/clients/CourseClient.java
@@ -3,6 +3,8 @@ package com.victorxavier.course_platform.authuser.clients;
 import com.victorxavier.course_platform.authuser.dtos.CourseDTO;
 import com.victorxavier.course_platform.authuser.dtos.ResponsePageDTO;
 import com.victorxavier.course_platform.authuser.services.UtilsService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,9 +18,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Slf4j
 @Component
@@ -33,16 +33,18 @@ public class CourseClient {
     @Value("${course_platform.api.url.course}")
     String REQUEST_URl_COURSE;
 
+    @Retry(name = "retryInstance", fallbackMethod = "retryFallback")
+    @CircuitBreaker(name = "circuitBreakerInstance")
     public Page<CourseDTO> getAllCoursesByUser(UUID userId, Pageable pageable) {
         List<CourseDTO> searchResult = null;
         Page<CourseDTO> page = Page.empty(pageable);
         String url = REQUEST_URl_COURSE + utilsService.createUrl(userId, pageable);
         log.debug("Request URL: {} ", url);
         log.info("Request URL: {} ", url);
+
         try {
             ParameterizedTypeReference<ResponsePageDTO<CourseDTO>> responseType =
                     new ParameterizedTypeReference<ResponsePageDTO<CourseDTO>>() {};
-
             ResponseEntity<ResponsePageDTO<CourseDTO>> result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
 
             if (result.getBody() != null) {
@@ -57,17 +59,26 @@ public class CourseClient {
                 }
             } else {
                 log.warn("Response body is null for URL: {}", url);
-                page = new PageImpl<>(Collections.emptyList(), pageable, 0);
+                page = createEmptyPage(pageable);
             }
         } catch (HttpStatusCodeException e){
             log.error("Error Request /courses userId {}: Status Code: {}, Body: {}", userId, e.getStatusCode(), e.getResponseBodyAsString(), e);
-            page = new PageImpl<>(Collections.emptyList(), pageable, 0);
+            page = createEmptyPage(pageable);
         } catch (Exception e) {
             log.error("Unexpected error during request /courses userId {}: {}", userId, e.getMessage(), e);
-            page = new PageImpl<>(Collections.emptyList(), pageable, 0);
+            page = createEmptyPage(pageable);
         }
         log.info("Ending request /courses userId {} ", userId);
         return page;
     }
 
+    public Page<CourseDTO> retryFallback(UUID userId, Pageable pageable, Exception ex) {
+        log.error("Fallback triggered for getAllCoursesByUser. UserId: {}, Cause: {}",
+                userId, ex.getMessage(), ex);
+        return createEmptyPage(pageable);
+    }
+
+    private Page<CourseDTO> createEmptyPage(Pageable pageable) {
+        return new PageImpl<>(Collections.emptyList(), pageable, 0);
+    }
 }
